f40d93b9fcbd436499e6d41402c03f03c9c51014
dbasobrinho <166563862+dbasobrinho@users.noreply.github.com>
2024-06-05 14:32:37 -0300

    Add files via upload

--- dg.sql
+++ dg.sql
@@ -3,10 +3,10 @@
 -- | Objetivo   : GAP Replicacao Data Guard                                                    |
 -- | Criador    : Roberto Fernandes Sobrinho                                                   |
 -- | Data       : 30/05/2024                                                                   |
--- | Exemplo    : @dg.sql                                                                      |  
+-- | Exemplo    : @dg.sql                                                                      |
 -- | Arquivo    : dg.sql                                                                       |
--- | Referncia  :                                                                              | 
--- | Modificacao:                                                                              |
+-- | Referncia  :                                                                              |
+-- | Modificacao: 1.1 - dbasobirnho - Ajustinho de nada, incluido o MAX                        |
 -- +-------------------------------------------------------------------------------------------+
 -- |                                                                https://dbasobrinho.com.br |
 -- +-------------------------------------------------------------------------------------------+
@@ -18,15 +18,15 @@ EXEC dbms_application_info.set_module( module_name => 'd[dg.sql]', action_name =
 COLUMN current_instance NEW_VALUE current_instance NOPRINT;
 SELECT rpad(sys_context('USERENV', 'INSTANCE_NAME'), 17) current_instance FROM dual;
 COLUMN DG_CONF NEW_VALUE DG_CONF NOPRINT;
-select value DG_CONF from v$parameter a where name = 'log_archive_config'; 
+select value DG_CONF from v$parameter a where name = 'log_archive_config';
 SET TERMOUT ON;
-PROMPT 
+PROMPT
 PROMPT +-------------------------------------------------------------------------------------------+
 PROMPT | https://github.com/dbasobrinho/g_gold/blob/main/dg.sql                                    |
 PROMPT +-------------------------------------------------------------------------------------------+
 PROMPT | Script   : GAP Replicacao Data Guard                             +-+-+-+-+-+-+-+-+-+-+-+  |
 PROMPT | Instancia: &current_instance                                     |d|b|a|s|o|b|r|i|n|h|o|  |
-PROMPT | Versao   : 1.0                                                   +-+-+-+-+-+-+-+-+-+-+-+  |
+PROMPT | Versao   : 1.1                                                   +-+-+-+-+-+-+-+-+-+-+-+  |
 PROMPT +-------------------------------------------------------------------------------------------+
 PROMPT | DG_CONF  : &DG_CONF
 PROMPT +-------------------------------------------------------------------------------------------+
@@ -36,13 +36,13 @@ SET ECHO        OFF
 SET FEEDBACK    10
 SET HEADING     ON
 SET LINES       188
-SET PAGES       300 
+SET PAGES       300
 SET TERMOUT     ON
 SET TIMING      OFF
 SET TRIMOUT     ON
 SET TRIMSPOOL   ON
 SET VERIFY      OFF
-CLEAR COLUMNS 
+CLEAR COLUMNS
 CLEAR BREAKS
 CLEAR COMPUTES
 
@@ -66,7 +66,11 @@ SET FEEDBACK    off
 PROMPT +-------------------------------------------------------------------------------------------+
 PROMPT | Status FUZZY Datafiles
 PROMPT +-------------------------------------------------------------------------------------------+
-column fuzzy format a6 heading 'fuzzy'
+COLUMN STATUS                FORMAT a10        HEAD 'STATUS'             justify CENTER
+COLUMN FUZZY                 FORMAT a08        HEAD 'FUZZY'              justify CENTER
+COLUMN CHECKPOINT_CHANGE     FORMAT a25        HEAD 'CHECKPOINT CHANGE'  justify CENTER 
+COLUMN CHECKPOINT_TIME       FORMAT a25        HEAD 'CHECKPOINT TIME'    justify CENTER 
+COLUMN CNT                   FORMAT 9999999    HEAD 'TOTAL'              justify CENTER 
 select status,to_char(checkpoint_change#) checkpoint_change
       ,to_char(checkpoint_time, 'dd-mon-yyyy hh24:mi:ss') as checkpoint_time
       ,count(*) cnt ,fuzzy
@@ -74,47 +78,48 @@ select status,to_char(checkpoint_change#) checkpoint_change
  group by status,checkpoint_change#,checkpoint_time,fuzzy
  order by status,checkpoint_change#,checkpoint_time
 /
+ 
 SET FEEDBACK    on
 PROMPT .
 PROMPT .
 PROMPT +-------------------------------------------------------------------------------------------+
-PROMPT | Data Guard GAP Status 
+PROMPT | Data Guard GAP Status
 PROMPT +-------------------------------------------------------------------------------------------+
 SET FEEDBACK    off
 select  case when ARC_DIFF <= 3 then ':)'  when ARC_DIFF > 3 AND ARC_DIFF <=8  then ':|' ELSE ':('  END ST
 ,  z.DATABASE_ROLE, z.PROTECTION_MODE, z.SWITCHOVER_STATUS, z.name, z.thread, z.STANDBY_LAST_RECEIVED
 ,  z.STANDBY_LAST_APPLIED, z.STANDBY_DT_LAST_APP, /*z.data_atual,*/ z.MINUTOS, z.ARC_DIFF
 from (
-SELECT /*+ PARALLEL(8) */   
+SELECT /*+ PARALLEL(8) */
        c.DATABASE_ROLE,
        c.PROTECTION_MODE,
        C.SWITCHOVER_STATUS,
-	   a.DEST_ID,
-	  (select nvl2(xx.name,xx.DEST_ID||' - '||xx.name,null)
-	     from v$archived_log xx 
-		where xx.DEST_ID = a.DEST_ID 
-		  and xx.resetlogs_change#=(SELECT resetlogs_change# FROM v$database)
-		  and SEQUENCE#  = (select max(yy.SEQUENCE#) 
-		                      from v$archived_log yy 
-							 where yy.resetlogs_change#=(SELECT resetlogs_change# FROM v$database)
-							  and yy.DEST_ID = xx.DEST_ID)) as name,
-	   a.thread# thread,
+           a.DEST_ID,
+          (select max(nvl2(xx.name,xx.DEST_ID||' - '||xx.name,null))
+             from v$archived_log xx
+                where xx.DEST_ID = a.DEST_ID
+                  and xx.resetlogs_change#=(SELECT resetlogs_change# FROM v$database)
+                  and SEQUENCE#  = (select max(yy.SEQUENCE#)
+                                      from v$archived_log yy
+                                                         where yy.resetlogs_change#=(SELECT resetlogs_change# FROM v$database)
+                                                          and yy.DEST_ID = xx.DEST_ID)) as name,
+           a.thread# thread,
        b. last_seq STANDBY_LAST_RECEIVED ,
        a.applied_seq STANDBY_LAST_APPLIED,
        TO_CHAR(a.last_app_timestamp,'DD/MM/YYYY HH24:MI:SS') as STANDBY_DT_LAST_APP,
            TO_CHAR(sysdate,'DD/MM/YYYY HH24:MI:SS') as data_atual,
            (sysdate - a.last_app_timestamp) *24*60  as MINUTOS,
-       b.last_seq - a.applied_seq ARC_DIFF 
-FROM   (SELECT /*+ PARALLEL(8) */ 
+       b.last_seq - a.applied_seq ARC_DIFF
+FROM   (SELECT /*+ PARALLEL(8) */
                DEST_ID,thread#,
-               Max(sequence#) applied_seq, 
+               Max(sequence#) applied_seq,
                Max(next_time) last_app_timestamp
         FROM   gv$archived_log
         WHERE  applied = 'YES'
                 and resetlogs_change#=(SELECT resetlogs_change# FROM v$database)
         GROUP  BY DEST_ID,thread#) a,
-       (SELECT /*+ PARALLEL(8) */ 
-	           DEST_ID,thread#,
+       (SELECT /*+ PARALLEL(8) */
+                   DEST_ID,thread#,
                Max (sequence#) last_seq
         FROM   gv$archived_log
                 where resetlogs_change#=(SELECT resetlogs_change# FROM v$database)
